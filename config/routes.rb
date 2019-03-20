c3eb8e9e9c85df446ad47bcbf492ef082e45e34e
weihansu <weihan.sw@gmail.com>
2019-03-20 18:42:58 +0000

    remove update except for categories in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :show]
   end
 
 
