ed3c6b9f6539962bfa9c508595674c8e90c9e43d
weihansu <weihan.sw@gmail.com>
2019-03-21 15:28:58 +0000

    add sessions MVC

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully Logged In!'
+      redirect_to root_path
+    else
+      flash[:warning] = 'Invalid Email or Password'
+      redirect_to '/login'
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully Logged Out!'
+    redirect_to '/login'
+
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
 
     # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
     @user.email.downcase!
@@ -17,17 +23,17 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       # If user fails model validation - probably a bad password or duplicate email:
-      flash[:warning] = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
-      render :new
+      flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
+      redirect_to '/signup'
     end
   end
 
-private
+# private
 
-  def user_params
-    # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
-    # that can be submitted by a form to the user model #=> require(:user)
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+#   def user_params
+#     # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
+#     # that can be submitted by a form to the user model #=> require(:user)
+#     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+#   end
 
 end
