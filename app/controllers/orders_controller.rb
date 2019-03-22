110a3670aa91c751c23db1244dc444f2da74cfbe
weihansu <weihan.sw@gmail.com>
2019-03-22 16:10:26 +0000

    add current user info to stripe's transaction

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
     Stripe::Charge.create(
       source:      params[:stripeToken],
       amount:      cart_subtotal_cents,
-      description: "Khurram Virani's Jungle Order",
+      description: "#{current_user.first_name.titleize} #{current_user.last_name.titleize}'s Jungle Order",
       currency:    'cad'
     )
   end
