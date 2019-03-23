3af19908f7631b6fae44b16d0bb8155dd09905f8
weihansu <weihan.sw@gmail.com>
2019-03-23 18:01:44 +0000

    add validation to rating's model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,7 @@ class Rating < ActiveRecord::Base
   belongs_to :product
 
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :description, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
 end
